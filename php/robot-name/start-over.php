3e01e5bfcac8f7d7393588b475f96c23d9b47503
Luke Logan <llogan382@gmail.com>
2018-07-24 06:27:25 -0400

    Maybe its best to start over?

---
+++ php/robot-name/start-over.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * create a class called robot
+ * this class will return an object from the second class
+ * 
+ * second class will run the methods, store the array
+ * of unique values
+ * 
+ */
